9940a4b7a9a4c9d0d0814018c6d832f6a98547c1
Annie <anqi.chen@thoughtworks.com>
2020-08-03 20:34:46 +0800

    [anqi] commit E1

--- 1/src/main/java/com/twu/refactoring/Order.java
+++ 1/src/main/java/com/twu/refactoring/Order.java
@@ -3,25 +3,43 @@ package com.twu.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    String username;
+    String address;
+    List<LineItem> lineItems;
 
-    public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+    public Order(String name, String address, List<LineItem> lineItems) {
+        this.username = name;
+        this.address = address;
+        this.lineItems = lineItems;
     }
 
     public String getCustomerName() {
-        return nm;
+        return username;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return address;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return lineItems;
+    }
+
+    public double totalSalesTax(double tax) {
+        double totalSalesTax = 0d;
+        for (LineItem lineItem : getLineItems()) {
+            double salesTax = lineItem.totalAmount() * tax;
+            totalSalesTax += salesTax;
+        }
+        return totalSalesTax;
+    }
+
+    public double totalSales(double tax) {
+        double totalSales = 0d;
+        for (LineItem lineItem : getLineItems()) {
+            totalSales += lineItem.totalAmount();
+        }
+        totalSales += totalSalesTax(tax);
+        return totalSales;
     }
 }

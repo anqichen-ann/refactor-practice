19184b56ad7fdb30d10f6a56258ea2d3f797bfa7
Annie <anqi.chen@thoughtworks.com>
2020-08-03 21:33:17 +0800

    [anqi] commit E3

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -2,39 +2,28 @@ package com.twu.refactoring;
 
 public class Direction {
     private final char direction;
+    private final char[] directionArray = {'N','E','S','W'};
 
     public Direction(char direction) {
         this.direction = direction;
     }
 
     public Direction turnRight() {
-        switch (direction) {
-            case 'N':
-                return new Direction('E');
-            case 'S':
-                return new Direction('W');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
+        for (int i = 0; i < directionArray.length; i++) {
+            if (direction == directionArray[i]) {
+                return new Direction(directionArray[(i+1)%4]);
+            }
         }
+        throw new IllegalArgumentException();
     }
 
     public Direction turnLeft() {
-        switch (direction) {
-            case 'N':
-                return new Direction('W');
-            case 'S':
-                return new Direction('E');
-            case 'E':
-                return new Direction('N');
-            case 'W':
-                return new Direction('S');
-            default:
-                throw new IllegalArgumentException();
+        for (int i = 0; i < directionArray.length; i++) {
+            if (direction == directionArray[i]) {
+                return new Direction(directionArray[(i+3)%4]);
+            }
         }
+        throw new IllegalArgumentException();
     }
 
     @Override

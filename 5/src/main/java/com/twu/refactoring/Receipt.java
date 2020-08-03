9d80fba3d372fe29a5d7ade1b94511acd08b65cc
Annie <anqi.chen@thoughtworks.com>
2020-08-03 22:19:46 +0800

    [anqi] commit E5

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -3,12 +3,9 @@ package com.twu.refactoring;
 public class Receipt {
     private static final int FIXED_CHARGE = 50;
     private static final double PEAK_TIME_MULTIPLIER = 1.2;
-    private static final double OFF_PEAK_MULTIPLIER = 1.0;
     private static final int RATE_CHANGE_DISTANCE = 10;
-    private static final int PRE_RATE_CHANGE_NON_AC_RATE = 15;
-    private static final int POST_RATE_CHANGE_NON_AC_RATE = 12;
-    private static final int PRE_RATE_CHANGE_AC_RATE = 20;
-    private static final int POST_RATE_CHANGE_AC_RATE = 17;
+    private static final int[] NON_AC_RATE = {15, 12};
+    private static final int[] AC_RATE = {20, 17};
     private static final double SALES_TAX_RATE = 0.1;
 
     private final Taxi taxi;
@@ -18,22 +15,24 @@ public class Receipt {
     }
 
     public double getTotalCost() {
-        double totalCost = 0;
+        double totalCost;
 
-        // fixed charges
-        totalCost += FIXED_CHARGE;
-
-        // taxi charges
         int totalKms = taxi.getTotalKms();
-        double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER : OFF_PEAK_MULTIPLIER;
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_AC_RATE * peakTimeMultiple;
+            totalCost = ConditionCost(totalKms, AC_RATE);
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE, totalKms) * PRE_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE) * POST_RATE_CHANGE_NON_AC_RATE * peakTimeMultiple;
+            totalCost = ConditionCost(totalKms, NON_AC_RATE);
         }
 
-        return totalCost * (1 + SALES_TAX_RATE);
+        return (totalCost + FIXED_CHARGE) * (1 + SALES_TAX_RATE);
     }
-}
+
+    public double ConditionCost(int totalKms, int[] RATE) {
+        double result = 0;
+        if (totalKms > RATE_CHANGE_DISTANCE) {
+            result += RATE_CHANGE_DISTANCE*RATE[0]+(totalKms-RATE_CHANGE_DISTANCE)*RATE[1];
+        } else {
+            result += totalKms*RATE[0];
+        }
+        return taxi.isPeakTime() ? result*PEAK_TIME_MULTIPLIER : result;
+    }}
